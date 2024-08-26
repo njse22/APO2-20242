ea0d1c46b604431a01a3961eba8022d7d452d065
Nicolás Javier Salazar Echeverry <38898558+njse22@users.noreply.github.com>
2024-08-26 17:15:44 -0500

    Update PersonCCComparator.java

--- Semana4/Serializacion/src/main/java/model/PersonCCComparator.java
+++ Semana4/Serializacion/src/main/java/model/PersonCCComparator.java
@@ -6,6 +6,6 @@ public class PersonCCComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-        return o1.compareTo(o2);
+        return o1.getCc().compareTo(o2.getCc());
     }
 }
